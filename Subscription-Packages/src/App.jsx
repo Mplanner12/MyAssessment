b101f0ef68feae5b12765efa724344c7aa86766c
Mustapha Shuaibu <119011957+Mplanner12@users.noreply.github.com>
2024-04-17 00:02:07 +0100

    first commit

---
+++ Subscription-Packages/src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import Subscription from "./components/Subscription";
+
+function App() {
+  return <Subscription />;
+}
+
+export default App;
---
+++ Subscription-Packages/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.html",
+    "./src/**/*.js",
+    "./src/**/*.jsx",
+    "./src/components/**/*.{html,js, jsx}",
+    // "./src/pages/**/*.{html,js,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
